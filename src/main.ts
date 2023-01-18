d2aa4f83ecd2d8c6c9b850c2bce89ef2d77e2510
verne1997 <sourjya@refactor.academy>
2023-01-18 16:06:21 +0530

    added passport startegy and auth modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalInterceptor } from './interceptors/global.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalInterceptors(new GlobalInterceptor());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(6001);
 }
 bootstrap();
